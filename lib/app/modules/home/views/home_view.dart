a855450ee5724984fdf0be01f5c21685eeb5ffe3
king-kazu39 <magmanakazato@gmail.com>
2022-10-26 23:38:25 +0900

    カードのヘッダー部分作成

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:favoritism_communication/app/modules/components/nav_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
@@ -7,16 +8,61 @@ class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: NavBar(
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      appBar: const NavBar(
         backgroundColor: Colors.pinkAccent,
         hasBackButton: false,
         title: 'ホーム',
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0)),
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              elevation: 0,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: const [
+                            CircleAvatar(
+                              // fixme: ユーザのプロフィール画像がFirestorageから取れるようになったら修正
+                              child: Icon(CupertinoIcons.person),
+                            ),
+                            SizedBox(width: 15),
+                            Text(
+                              // fixme: ユーザ名がFiresstoreから取れるようになったら修正
+                              'ユーザネーム',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.blueAccent,
+                            shape: const StadiumBorder(
+                                side: BorderSide(color: Colors.blueAccent)),
+                            elevation: 0,
+                          ),
+                          onPressed: () {},
+                          child: Text('フォローする'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
