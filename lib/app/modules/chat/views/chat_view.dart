0b36d5956e9db77270fda703f083e6e29aa8b0f1
“motaken” <kenta.mori2019@gmail.com>
2022-10-28 10:25:25 +0900

    トーク画面TOPカード作成

--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:favoritism_communication/app/modules/components/nav_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -16,11 +18,65 @@ class ChatView extends GetView<ChatController> {
         hasBackButton: false,
       ),
       body: Center(
-        child: Text(
-          'ChatView is working',
-          style: TextStyle(fontSize: 20),
+        child: TalkMemberCard(
+          //TODO: サンプルのため後で削除
+          roomName: '123456789012345678901234567890', 
+          mostRecentMessage: '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890',
+          profileImageURL: null,
+          ),
+      ),
+    );
+  }
+}
+
+class TalkMemberCard extends GetView<ChatController> {
+  const TalkMemberCard({Key? key, required this.roomName, required this.mostRecentMessage, required this.profileImageURL}) : super(key: key);
+
+  final String roomName;
+  final String mostRecentMessage;
+  final profileImageURL;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0),),
+      child: Padding(
+        padding: const EdgeInsets.all(15), 
+        child: Row(
+          children: [
+            CircleAvatar(
+              //TODO:firebase接続後に動作確認
+              //profileImageURLがあればその画像を表示、なければグレー背景でpersonアイコンを表示
+              foregroundImage: profileImageURL != null ? NetworkImage(profileImageURL) : null, 
+              backgroundColor: Colors.grey,
+              child: const Icon(Icons.person, color: Colors.white,),
+            ), 
+            const SizedBox(width: 20,),
+            Flexible(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    roomName, 
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                  Text(
+                    mostRecentMessage,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
+
 }
