6123eb8e17e0a4cb0ae5e682aa0692233014cdd1
king-kazu39 <magmanakazato@gmail.com>
2022-11-01 23:43:15 +0900

    カードの大枠できた

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -20,7 +20,11 @@ class HomeView extends GetView<HomeController> {
           children: [
             Card(
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0)),
+                borderRadius: BorderRadius.circular(15.0),
+                side: BorderSide(
+                  color: Colors.grey.shade300,
+                ),
+              ),
               margin: EdgeInsets.symmetric(horizontal: 20),
               elevation: 0,
               child: Column(
@@ -32,9 +36,13 @@ class HomeView extends GetView<HomeController> {
                       children: [
                         Row(
                           children: const [
+                            // fixme: あとでコンポーネント化する
                             CircleAvatar(
-                              // fixme: ユーザのプロフィール画像がFirestorageから取れるようになったら修正
-                              child: Icon(CupertinoIcons.person),
+                              backgroundColor: Colors.grey,
+                              child: Icon(
+                                Icons.person,
+                                color: Colors.white,
+                              ),
                             ),
                             SizedBox(width: 15),
                             Text(
@@ -55,6 +63,106 @@ class HomeView extends GetView<HomeController> {
                           onPressed: () {},
                           child: Text('フォローする'),
                         ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                        left: 15.0, right: 15.0, bottom: 15.0),
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          '好きなカテゴリ',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Wrap(
+                          runSpacing: 4.0,
+                          spacing: 4.0,
+                          children: [
+                            Chip(
+                              label: Text('旅行'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('映画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('漫画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('漫画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('漫画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('漫画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('漫画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('漫画'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('ホゲホゲホゲ。これくらいの長さまでは書いてOK'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('その他はユーザが決めれる'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                            Chip(
+                              label: Text('ホゲホゲホゲホゲ'),
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              backgroundColor:
+                                  Color.fromARGB(255, 245, 245, 222),
+                            ),
+                          ],
+                        )
                       ],
                     ),
                   ),
