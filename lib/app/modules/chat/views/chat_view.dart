c5c17e3ac9133928226d5e830c61c6cc4844547b
“motaken” <kenta.mori2019@gmail.com>
2022-11-02 15:03:37 +0900

    Github上でのlinterのエラーを解消

--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -1,6 +1,5 @@
 import 'package:favoritism_communication/app/modules/components/nav_bar.dart';
 import 'package:favoritism_communication/app/modules/chat/views/talk_member_card.dart';
-import 'package:favoritism_communication/app/modules/create_chat_group/views/create_chat_group_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
