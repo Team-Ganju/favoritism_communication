892668bd2f42b5e3f57e4820a71ae6ae200741b7
Yuki Nishimura <yukinkopen@gmail.com>
2022-11-08 13:33:07 +0900

    ListViewを利用してアイテムを表示 ランダムに生成したカテゴリリストを利用

--- lib/app/components/pages/home/views/home_view.dart
+++ lib/app/components/pages/home/views/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 import '../../../molecules/user_card.dart';
+import 'dart:math' as math;
 
 class HomeView extends GetView<HomeController> {
   static const tempCategoryNameList = ['観光地', '映画', '本', '音楽', 'ご飯', 'スポーツ', 'ホゲホゲ', 'カテゴリは10個まで', 'これくらいの長さまでは書いてOK。20字程度。', 'その他はユーザが決めれる'];
@@ -10,11 +11,19 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     // todo 外部からユーザデータを取得
-    List<CategoryData> tempCategoryList = [];
-    for (var item in tempCategoryNameList) {
-      tempCategoryList.add(CategoryData(item, const Color.fromRGBO(255, 250, 205, 1)));
-    }
-    var tempUserCardData = UserCardData("ユーザネーム", tempCategoryList);
+    var userName = ['Kaz', 'Motaken', 'Raiki', 'Nisshy'];
+    var userCardList = userName.map((item) {
+      return UserCard(
+        context,
+        UserCardData(
+          item,
+          getRandomCategoryList().map((item) {
+            return CategoryData(item, const Color.fromRGBO(255, 250, 205, 1));
+          }).toList()
+        ),
+        controller
+      );
+    }).toList();
 
     return Scaffold(
       backgroundColor: Colors.grey[100],
@@ -22,16 +31,30 @@ class HomeView extends GetView<HomeController> {
         backgroundColor: Colors.pinkAccent,
         title: 'ホーム',
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            UserCard(context, tempUserCardData, controller),
-            UserCard(context, tempUserCardData, controller),
-            UserCard(context, tempUserCardData, controller)
-          ],
+      body: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
+          children: userCardList
         ),
-      ),
-    );
+      );
   }
+}
+
+List<String> getRandomCategoryList() {
+  // ランダムなカテゴリを返す
+  var categories = ['観光地', '映画', '本', '音楽', 'ご飯', 'スポーツ', 'ホゲホゲ', 'カテゴリは10個まで', 'これくらいの長さまでは書いてOK。20字程度。', 'その他はユーザが決めれる'];
+  var indexList = [];
+  for (var i = 0; i < categories.length; i++) {
+    indexList.add(i);
+  }
+  var rand = math.Random();
+  for (var i = 0; i < categories.length; i++) {
+    var first = rand.nextInt(categories.length);
+    var second = rand.nextInt(categories.length);
+    var temp = indexList[first];
+    indexList[first] = indexList[second];
+    indexList[second] = temp;
+  }
+  return indexList.map((item) {
+    return categories[item];
+  }).toList();
 }
