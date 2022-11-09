3038c608414b935c8aba0d6dd8ad738ead3a7336
king-kazu39 <magmanakazato@gmail.com>
2022-11-10 03:04:26 +0900

    リファクタリング

--- lib/app/components/atoms/atoms.dart
+++ lib/app/components/atoms/atoms.dart
@@ -1,3 +1,4 @@
 //atoms配下のファイルを一括でimportできるようにするためのファイル
 export 'back_button.dart';
 export 'custom_chip.dart';
+export 'follow_button.dart';
--- lib/app/components/pages/home/views/home_view.dart
+++ lib/app/components/pages/home/views/home_view.dart
@@ -1,29 +1,14 @@
-import 'package:favoritism_communication/app/components/organisms/nav_bar.dart';
+import 'package:favoritism_communication/app/components/atoms/atoms.dart';
+import 'package:favoritism_communication/app/components/organisms/organisms.dart';
+import 'package:favoritism_communication/app/dummy_data/user_card_dummy_data.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
-import '../../../molecules/user_card.dart';
-import 'dart:math' as math;
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // todo 外部からユーザデータを取得
-    var userName = ['Kaz', 'Motaken', 'Raiki', 'Nisshy'];
-    var userCardList = userName.map((item) {
-      return UserCard(
-        context,
-        UserCardData(
-          item,
-          getRandomCategoryList().map((item) {
-            return CategoryData(item, getRandomColor());
-          }).toList()
-        ),
-        controller
-      );
-    }).toList();
-
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: const NavBar(
@@ -31,48 +16,49 @@ class HomeView extends GetView<HomeController> {
         title: 'ホーム',
       ),
       body: NotificationListener<ScrollNotification>(
-        child: ListView(
-          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 8),
-          children: userCardList
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView.separated(
+            itemCount: userCardList.length,
+            itemBuilder: (context, index) {
+              final UserCardData userCardData =
+                  controller.userCardDataList[index];
+              return CustomUserCard(
+                userCardData: userCardData,
+                followAction: Obx(
+                  () => controller.userCardDataList[index].isFollowed
+                      ? FollowButton(
+                          onPressed: () {
+                            controller.unFollow(userCardData);
+                          },
+                          backgroundColor: Colors.blueAccent,
+                          foregroundColor: Colors.white,
+                          isFollowed: userCardData.isFollowed,
+                        )
+                      : FollowButton(
+                          onPressed: () {
+                            controller.follow(userCardData);
+                          },
+                          backgroundColor: Colors.white,
+                          foregroundColor: Colors.blueAccent,
+                          isFollowed: userCardData.isFollowed,
+                        ),
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const SizedBox(height: 10),
+          ),
         ),
         onNotification: (ScrollNotification notification) {
-            print('OverscrollNotification:${notification is OverscrollNotification}');
-            print('maxScrollExtent:${notification.metrics.maxScrollExtent}');
-            print('pixels:${notification.metrics.pixels}');
-            print('outOfRange:${notification.metrics.outOfRange}');
+          print(
+              'OverscrollNotification:${notification is OverscrollNotification}');
+          print('maxScrollExtent:${notification.metrics.maxScrollExtent}');
+          print('pixels:${notification.metrics.pixels}');
+          print('outOfRange:${notification.metrics.outOfRange}');
 
-            return false;
-        }
+          return false;
+        },
       ),
     );
   }
-}
-
-List<String> getRandomCategoryList() {
-  var categories = ['観光地', '映画', '本', '音楽', 'ご飯', 'スポーツ', 'ホゲホゲ', 'カテゴリは10個まで', 'これくらいの長さまでは書いてOK。20字程度。', 'その他はユーザが決めれる'];
-  var indexList = [];
-  var rand = math.Random();
-  var categoryNum = rand.nextInt(categories.length);
-  categoryNum = categoryNum < 5 ? 5 : categoryNum;
-  for (var i = 0; i < categoryNum; i++) {
-    indexList.add(i);
-  }
-  for (var i = 0; i < categoryNum; i++) {
-    var first = rand.nextInt(categoryNum);
-    var second = rand.nextInt(categoryNum);
-    var temp = indexList[first];
-    indexList[first] = indexList[second];
-    indexList[second] = temp;
-  }
-  return indexList.map((item) {
-    return categories[item];
-  }).toList();
-}
-
-Color getRandomColor() {
-  var rand = math.Random();
-  var red = rand.nextInt(100) + 155;
-  var green = rand.nextInt(100) + 155;
-  var blue = rand.nextInt(100) + 155;
-  return Color.fromRGBO(red, green, blue, 1);
 }
