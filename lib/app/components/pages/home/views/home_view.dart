8f7f58a55203a3f66010dbc0a8728a7cfedcc5bc
king-kazu39 <magmanakazato@gmail.com>
2022-11-12 23:43:02 +0900

    スクロールがあるビューをSmartRefresherを使ってテンプレート化

--- lib/app/components/pages/home/views/home_view.dart
+++ lib/app/components/pages/home/views/home_view.dart
@@ -1,8 +1,9 @@
 import 'package:favoritism_communication/app/components/atoms/atoms.dart';
 import 'package:favoritism_communication/app/components/organisms/organisms.dart';
+import 'package:favoritism_communication/app/components/templates/custom_smartrefresher.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pull_to_refresh/pull_to_refresh.dart';
+// import 'package:pull_to_refresh/pull_to_refresh.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -20,63 +21,61 @@ class HomeView extends GetView<HomeController> {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Scrollbar(
-              child: SmartRefresher(
-                controller: controller.refreshController,
-                enablePullDown: false,
-                enablePullUp: true,
-                onLoading: () {
-                  controller.onLoading();
-                },
-                child: SingleChildScrollView(
-                  child: Obx(
-                    () => controller.userCardDataList.isNotEmpty
-                        ? ListView.separated(
-                            physics: const NeverScrollableScrollPhysics(),
-                            shrinkWrap: true,
-                            itemCount: controller.userCardDataList.length,
-                            itemBuilder: (context, index) {
-                              final UserCardData userCardData =
-                                  controller.userCardDataList[index];
-                              return UserCard(
-                                userCardData: userCardData,
-                                followAction: Obx(
-                                  () => controller
-                                          .userCardDataList[index].isFollowed
-                                      ? FollowButton(
-                                          onPressed: () {
-                                            controller.unFollow(userCardData);
-                                          },
-                                          backgroundColor: Colors.blueAccent,
-                                          foregroundColor: Colors.white,
-                                          isFollowed: userCardData.isFollowed,
-                                        )
-                                      : FollowButton(
-                                          onPressed: () {
-                                            controller.follow(userCardData);
-                                          },
-                                          backgroundColor: Colors.white,
-                                          foregroundColor: Colors.blueAccent,
-                                          isFollowed: userCardData.isFollowed,
-                                        ),
-                                ),
-                              );
-                            },
-                            separatorBuilder: (context, index) =>
-                                const SizedBox(height: 10),
-                          )
-                        : Column(
-                            children: const [
-                              SizedBox(height: 30),
-                              Center(
-                                child: Text(
-                                  '検索結果がありません',
-                                  style: TextStyle(fontSize: 18),
-                                ),
+            child: CustomSmartRefresher(
+              refreshController: controller.refreshController,
+              enablePullDown: false,
+              enablePullUp: true,
+              onLoading: () {
+                controller.onLoading();
+              },
+              child: SingleChildScrollView(
+                child: Obx(
+                  () => controller.userCardDataList.isNotEmpty
+                      ? ListView.separated(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: controller.userCardDataList.length,
+                          itemBuilder: (context, index) {
+                            final UserCardData userCardData =
+                                controller.userCardDataList[index];
+                            return UserCard(
+                              userCardData: userCardData,
+                              followAction: Obx(
+                                () => controller
+                                        .userCardDataList[index].isFollowed
+                                    ? FollowButton(
+                                        onPressed: () {
+                                          controller.unFollow(userCardData);
+                                        },
+                                        backgroundColor: Colors.blueAccent,
+                                        foregroundColor: Colors.white,
+                                        isFollowed: userCardData.isFollowed,
+                                      )
+                                    : FollowButton(
+                                        onPressed: () {
+                                          controller.follow(userCardData);
+                                        },
+                                        backgroundColor: Colors.white,
+                                        foregroundColor: Colors.blueAccent,
+                                        isFollowed: userCardData.isFollowed,
+                                      ),
                               ),
-                            ],
-                          ),
-                  ),
+                            );
+                          },
+                          separatorBuilder: (context, index) =>
+                              const SizedBox(height: 10),
+                        )
+                      : Column(
+                          children: const [
+                            SizedBox(height: 30),
+                            Center(
+                              child: Text(
+                                '検索結果がありません',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                          ],
+                        ),
                 ),
               ),
             ),
