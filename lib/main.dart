f3865e18239d3d592a9f283fc01b38d507939670
king-kazu39 <magmanakazato@gmail.com>
2022-11-13 15:10:30 +0900

    initalBindingを消してしまったので追加

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
           Locale("ja", "JP"),
         ],
         initialRoute: AppPages.initial,
+        initialBinding: InitialBinding(),
         getPages: AppPages.routes,
       ),
     );
