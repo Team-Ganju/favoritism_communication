276ae9c9ff00c47e3864ff96c705bda86013627f
“motaken” <kenta.mori2019@gmail.com>
2022-11-16 00:34:56 +0900

    ListTileにonTapのプロパティを追加

--- lib/app/components/organisms/search_result_list_tile.dart
+++ lib/app/components/organisms/search_result_list_tile.dart
@@ -6,11 +6,13 @@ class SeachResultListTile extends StatelessWidget {
     required this.index,
     required this.text,
     this.onPressed,
+    this.onTap,
   });
 
   final int index;
   final String text;
   final VoidCallback? onPressed;
+  final VoidCallback? onTap;
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +24,8 @@ class SeachResultListTile extends StatelessWidget {
       title: Text(
         text,
       ),
+      //TODO: メンバー選択処理実装
+      onTap: onTap,
     );
   }
 }
