334aa2a9360abd9e45a1a0677824fac46e30509e
“motaken” <kenta.mori2019@gmail.com>
2022-11-30 04:56:43 +0900

    候補メンバーカードウィジェット追加

---
+++ lib/app/components/organisms/candidate_member_card.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class CandidateMemberCard extends StatelessWidget {
+  const CandidateMemberCard({
+    Key? key,
+    required this.name,
+    this.profileImageURL,
+    required this.isSelected,
+  }) : super(key: key);
+
+  final String name;
+  final String? profileImageURL;
+  final RxBool isSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+          side: BorderSide(
+            //FIXME: 各種項目の色はアプリ全体の色を管理するファイルから参照するように修正する
+            color: Colors.grey.shade300,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(15),
+          child: Row(
+            children: [
+              CircleAvatar(
+                //TODO:firebase接続後に動作確認
+                //profileImageURLがあればその画像を表示、なければグレー背景でpersonアイコンを表示
+                foregroundImage: profileImageURL != null
+                    ? NetworkImage(profileImageURL!)
+                    : null,
+                backgroundColor: Colors.grey,
+                child: const Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Flexible(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+              isSelected.value
+                  ? const Icon(
+                      Icons.check_circle,
+                      color: Colors.green,
+                    )
+                  : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/components/organisms/organisms.dart
+++ lib/app/components/organisms/organisms.dart
@@ -1,3 +1,4 @@
+export 'candidate_member_card.dart';
 export 'message_bar.dart';
 export 'user_card.dart';
 export 'nav_bar.dart';
