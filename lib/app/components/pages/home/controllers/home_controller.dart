ddaa2b130ea1793126036b08629f2efdb438db28
Yuki Nishimura <yukinkopen@gmail.com>
2022-12-01 02:21:29 +0900

    assignAllを利用する形に置換

--- lib/app/components/pages/home/controllers/home_controller.dart
+++ lib/app/components/pages/home/controllers/home_controller.dart
@@ -50,8 +50,7 @@ class HomeController extends GetxController {
   }
 
   void initTab(List<TabData> list) {
-    tabDataList.clear();
-    tabDataList.addAll(list);
+    tabDataList.assignAll(list);
     tabDataList.refresh();
   }
 
