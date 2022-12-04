38a18545d3095ac3f0d1c50ee27b64f742ef4e87
R.Noto <rikitonoto@gmail.com>
2022-12-04 16:27:59 +0900

    [デモページ]未使不要なオーバーライドメソッドを削除

--- experimental/pages/demologin/controllers/demologin_controller.dart
+++ experimental/pages/demologin/controllers/demologin_controller.dart
@@ -4,20 +4,6 @@ class DemologinController extends GetxController {
   //TODO: Implement DemologinController
 
   final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
 
   void increment() => count.value++;
 }
