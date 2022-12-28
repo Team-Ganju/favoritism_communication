cd99a2da47a973186262ec438d1fd9d8fdd626f7
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2022-12-28 23:17:23 +0900

    [デモページ]ホーム画面のたたき台作成

--- experimental/pages/demo_home/views/demo_home_view.dart
+++ experimental/pages/demo_home/views/demo_home_view.dart
@@ -8,13 +8,61 @@ class DemoHomeView extends GetView<DemoHomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(top: 50.0),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Text(
-              'ユーザID:${controller.authService.uid.val}'), //TODO: ログインユーザーのIDを表示
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                  'ユーザID:${controller.authService.uid.val}'), //TODO: ログインユーザーのIDを表示
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                  'ユーザー情報',
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildMenuItem(title: 'プロフィール情報登録'),
+                _buildMenuItem(title: 'プロフィール情報閲覧'),
+                _buildMenuItem(title: 'プロフィール情報編集'),
+                _buildMenuItem(title: 'プロフィール削除', textColor: Colors.red),
+              ],
+            ),
+          ),
+        ],
+      ),
+
+    );
+  }
+
+  Widget _buildMenuItem({
+    String? title,
+    Color? textColor,
+  }){
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(
+            color: Colors.grey,
+            width: 0.1,
+          ),
+          bottom: BorderSide(
+            color: Colors.grey,
+            width: 0.1,
+          ),
+        )
+      ),
+      child: ListTile(
+        title: title!=null ? Text(title) : null,
+        textColor: textColor ?? Colors.grey,
       ),
     );
   }
