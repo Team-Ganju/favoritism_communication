2209f903e3385b383f9b678ebd6cb1cf6a4f6666
RikitoNoto <56541594+RikitoNoto@users.noreply.github.com>
2023-01-14 02:24:28 +0900

    [デモページ]プロフィール作成画面のベース作成

--- experimental/pages/demo_profile_create/views/demo_profile_create_view.dart
+++ experimental/pages/demo_profile_create/views/demo_profile_create_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import 'package:favoritism_communication/app/components/atoms/custom_elevated_button.dart';
 import '../controllers/demo_profile_create_controller.dart';
 
 class DemoProfileCreateView extends GetView<DemoProfileCreateController> {
@@ -9,14 +10,82 @@ class DemoProfileCreateView extends GetView<DemoProfileCreateController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('DemoProfileCreateView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'DemoProfileCreateView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          children: [
+            Text("プロフィール情報登録",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.grey,
+              child: const Icon(
+                Icons.person,
+                color: Colors.white,
+                size: 120,
+              ),
+              radius: 80,
+            ),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+
+                      decoration: InputDecoration(
+                        label: Text("ユーザ名"),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: DropdownButton(
+                      items: [
+                        DropdownMenuItem(child: Text("男")),
+                        DropdownMenuItem(child: Text("女")),
+                      ],
+
+                      onChanged: (arg) => {},
+                      // decoration: InputDecoration(
+                      //   label: Text("性別"),
+                      //   border: OutlineInputBorder(),
+                      // ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        label: Text("年代"),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("自己紹介"),
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+        CustomElevatedButton(
+          buttonText: '登録する',
+          initialActiveFlag: true,
+          onPressed: () => {},
+        )
+          ],
         ),
       ),
     );
