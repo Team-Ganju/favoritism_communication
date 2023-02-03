132beff5b675623369a83e21aab52bf6b66dc1c0
“motaken” <kenta.mori2019@gmail.com>
2023-02-04 03:28:31 +0900

    TalkRoomModel修正

--- lib/app/models/talk_room_model.dart
+++ lib/app/models/talk_room_model.dart
@@ -1,17 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class TalkRoomModel {
   String? roomId;
   String? roomName;
+  bool? isGroup;
+  String? lastMessage;
+  String? userId;
+  String? profileImage;
+  Timestamp? createdAt;
+  Timestamp? updatedAt;
   List<Map<String, dynamic>> messages = [];
 
   TalkRoomModel({
     required this.roomId,
     required this.roomName,
+    required this.isGroup,
+    required this.userId,
+    required this.lastMessage,
+    required this.profileImage,
+    required this.createdAt,
+    required this.updatedAt,
     required this.messages,
   });
 
   TalkRoomModel.fromJson(Map<String, dynamic> json) {
     roomId = json['roomId'];
     roomName = json['roomName'];
+    isGroup = json['isGroup'];
+    userId = json['userId'];
+    lastMessage = json['lastMessage'];
+    profileImage = json['profileImage'];
+    createdAt = json['createdAt'];
+    updatedAt = json['updatedAt'];
     messages = json['messages'];
   }
 
@@ -19,6 +39,12 @@ class TalkRoomModel {
     final data = <String, dynamic>{};
     data['roomId'] = roomId;
     data['roomName'] = roomName;
+    data['isGroup'] = isGroup;
+    data['userId'] = userId;
+    data['lastMessage'] = lastMessage;
+    data['profileImage'] = profileImage;
+    data['createdAt'] = createdAt;
+    data['updatedAt'] = updatedAt;
     data['messages'] = messages;
     return data;
   }
