3c7aefbbe4f95d1d6bbb1f6ed8747e16f93f90bf
“motaken” <kenta.mori2019@gmail.com>
2023-05-28 20:16:03 +0900

    微修正

--- lib/app/components/atoms/atoms.dart
+++ lib/app/components/atoms/atoms.dart
@@ -1,13 +1,13 @@
 //atoms配下のファイルを一括でimportできるようにするためのファイル
 export 'back_button.dart';
-export 'chat_bubble.dart';
 export 'custom_action_chip.dart';
+export 'custom_chat_bubble.dart';
 export 'custom_chip.dart';
 export 'custom_elevated_button.dart';
 export 'custom_footer.dart';
 export 'follow_button.dart';
 export 'message_form.dart';
+export 'no_image_circle_avator.dart';
 export 'pickup_list_item.dart';
 export 'tab_button.dart';
 export 'user_list_info_item.dart';
-export 'no_image_circle_avator.dart';
